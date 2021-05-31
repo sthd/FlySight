b5f0fe4b948fa30864e79ad2d067df06ca040713
Iddo <iddobar-haim@campus.technion.ac.il>
2021-05-31 10:28:30 +0300

    refactor to auxFunctions.py

---
+++ auxFunctions.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+WHITE_LEVEL = 255
+
+
+def greyscale_plot(image):
+    plt.imshow(image / WHITE_LEVEL, cmap='gray')
+    plt.show()
+
+
+def make_gaussian_kernel(size: int, sigma: float = 1) -> np.array:
+    x, y = np.meshgrid(np.linspace(-1, 1, size), np.linspace(-1, 1, size))
+    d = np.sqrt(x * x + y * y)
+    return np.exp(-((d - 0) ** 2 / (2.0 * sigma ** 2)))
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 import scipy.signal
 
-WHITE_LEVEL = 255
-
-def greyscale_plot(image):
-    plt.imshow(image / WHITE_LEVEL, cmap='gray')
-    plt.show()
-
-
-def make_gaussian_kernel(size: int, sigma: float = 1) -> np.array:
-    x, y = np.meshgrid(np.linspace(-1, 1, size), np.linspace(-1, 1, size))
-    d = np.sqrt(x * x + y * y)
-    return np.exp(-((d - 0) ** 2 / (2.0 * sigma ** 2)))
-
+from auxFunctions import greyscale_plot, make_gaussian_kernel
 
 if __name__ == '__main__':
     ker = make_gaussian_kernel(16, .5)
