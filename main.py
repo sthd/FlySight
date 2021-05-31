54ae8aca21c4702e79c904884e9b995e5a3f5b27
Iddo <iddobar-haim@campus.technion.ac.il>
2021-05-31 10:28:30 +0300

    iddos first push

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ def make_gaussian_kernel(size: int, sigma: float = 1) -> np.array:
     d = np.sqrt(x * x + y * y)
     return np.exp(-((d - 0) ** 2 / (2.0 * sigma ** 2)))
 
-ker = make_gaussian_kernel(16, .5)
-greyscale_plot(ker)
 
-img = cv2.imread('/Users/elior/PycharmProjects/FlySight/pic.jpg', cv2.IMREAD_GRAYSCALE)
+if __name__ == '__main__':
+    ker = make_gaussian_kernel(16, .5)
+    greyscale_plot(ker)
 
+    img = cv2.imread('pic.jpg', cv2.IMREAD_GRAYSCALE)
 
-greyscale_plot(img)
+    greyscale_plot(img)
 
-pr_sim = scipy.signal.convolve2d(img, ker)
-greyscale_plot(pr_sim)
+    pr_sim = scipy.signal.convolve2d(img, ker)
+    greyscale_plot(pr_sim)
