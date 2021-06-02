3f48b958c32e658c02775a81311571ca06b9f457
Iddo <iddobar-haim@campus.technion.ac.il>
2021-06-02 15:57:29 +0300

    PhotoreceptorImageConverter testing 14 tests passed

--- flyConvol.py
+++ flyConvol.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy
 import numpy as np
 
@@ -7,16 +6,54 @@ import numpy as np
 class PhotoreceptorImageConverter:
     def __init__(self, kernel: np.array, pic_shape: (int, int), photoreceptor_num: int):
         # Assumption: kernel_size is an odd number
-        self.horizontal = numpy.sqrt((pic_shape[1] * photoreceptor_num) / pic_shape[0]).round()
+        self.kernel = kernel
+        self.horizontal: int = numpy.sqrt((pic_shape[1] * photoreceptor_num) / pic_shape[0]).round().astype(int)
         if self.horizontal < 1:
             self.horizontal = 1
         self.vertical = round(photoreceptor_num / self.horizontal)
         self.h_stride = round(pic_shape[1] / self.horizontal)
         self.v_stride = round(pic_shape[0] / self.vertical)
-        self.h_padding = math.ceil((kernel.shape[0] - self.h_stride) / 2)
-        self.v_padding = math.ceil((kernel.shape[1] - self.v_stride) / 2)
+        self.h_padding = math.ceil((self.kernel.shape[0] - self.h_stride) / 2)
+        self.v_padding = math.ceil((self.kernel.shape[1] - self.v_stride) / 2)
 
     def apply(self, pic):
-        return 1
+        if self.h_padding < 0 or self.v_padding < 0:
+            padded_pic = self._slice_pic(pic)
+        else:
+            padded_pic = self._make_zero_padded_input(pic)
+        output = np.zeros((self.vertical, self.horizontal))
+        half_ker = (self.kernel.shape[0]-1)//2
+        self._convolve(half_ker, output, padded_pic)
+        return output
 
+    def _slice_pic(self, pic):
+        padded_pic = pic
+        if self.v_padding < 0:
+            padded_pic = padded_pic[-self.v_padding:self.v_padding, :]
+            self.v_padding = 0
+        if self.h_padding < 0:
+            padded_pic = padded_pic[:, -self.h_padding:self.h_padding]
+            self.h_padding = 0
+        return padded_pic
 
+    def _convolve(self, half_ker, output, padded_pic):
+        for i in range(self.vertical):
+            for j in range(self.horizontal):
+                input_row = i * self.v_stride + self.v_padding
+                input_column = j * self.h_stride + self.h_padding
+                output[i, j] = self._overlay_kernel(half_ker, input_row, input_column, padded_pic)
+
+    def _overlay_kernel(self, half_ker, i, j, padded_pic):
+        return np.multiply(self.kernel, self._kernel_sized_portion(padded_pic, half_ker, i, j)).sum()
+
+    @staticmethod
+    def _kernel_sized_portion(pic, half_ker_size, center_row, center_col):
+        return pic[center_row - half_ker_size:center_row + half_ker_size + 1,
+                   center_col - half_ker_size:center_col + half_ker_size + 1]
+
+    def _make_zero_padded_input(self, pic: np.array) -> np.array:
+        padded_pic = np.zeros((pic.shape[0] + 2 * self.v_padding, pic.shape[1] + 2 * self.h_padding))
+        for i in range(self.v_padding, padded_pic.shape[0] - self.v_padding):
+            for j in range(self.h_padding, padded_pic.shape[1] - self.h_padding):
+                padded_pic[i, j] = pic[i - self.v_padding, j - self.h_padding]
+        return padded_pic
--- test_flyConvol.py
+++ test_flyConvol.py
@@ -6,6 +6,11 @@ from flyConvol import PhotoreceptorImageConverter
 
 
 class TestConv(unittest.TestCase):
+    def assertEqualArrays(self, expected, actual):
+        self.assertEqual(expected.shape, actual.shape)
+        for e, a, in zip(np.nditer(expected), np.nditer(actual)):
+            self.assertEqual(e, a)
+
     def test_init(self):
         pr = PhotoreceptorImageConverter(np.ones((3, 3)), (6, 8), 48)
         self.assertEqual(8, pr.horizontal)
@@ -45,7 +50,6 @@ class TestConv(unittest.TestCase):
         self.assertEqual(-1, pr.h_padding)
         self.assertEqual(-1, pr.v_padding)
 
-
     def test_stride_2(self):
         pr = PhotoreceptorImageConverter(np.ones((3, 3)), (3, 4), 6)
         self.assertEqual(3, pr.horizontal)
@@ -70,6 +74,48 @@ class TestConv(unittest.TestCase):
         self.assertEqual(0, pr.v_padding)
         pic = np.array([[1]])
         self.assertEqual(1, pr.apply(pic))
+
+    def test_simple_application_different_value(self):
+        pr = PhotoreceptorImageConverter(np.array([[2]]), (1, 1), 1)
+        self.assertEqual(1, pr.horizontal)
+        self.assertEqual(1, pr.vertical)
+        self.assertEqual(0, pr.h_padding)
+        self.assertEqual(0, pr.v_padding)
+        pic = np.array([[1]])
+        self.assertEqual(2, pr.apply(pic))
+
+    def test_application_kernel_1x1_pic_2x2(self):
+        pic = np.ones((2, 2))
+        pr = PhotoreceptorImageConverter(np.array([[2]]), pic.shape, 4)
+        self.assertEqualArrays(2*np.ones((2, 2)), pr.apply(pic))
+
+    def test_kernel_3x3_pic_2x2_zero_padding(self):
+        pic = np.ones((2, 2))
+        pr = PhotoreceptorImageConverter(np.ones((3, 3)), pic.shape, 4)
+        self.assertEqualArrays(4 * np.ones((2, 2)), pr.apply(pic))
+
+    def test_negative_padding_apply(self):
+        pic = np.ones((3, 3))
+        pr = PhotoreceptorImageConverter(np.array([[2]]), pic.shape, 1)
+        self.assertEqualArrays(np.array([[2]]), pr.apply(pic))
+
+    def test_negative_padding_4x4(self):
+        pic = np.array([[0, 0, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 0, 0]])
+        pr = PhotoreceptorImageConverter(np.array([[1]]), pic.shape, 1)
+        self.assertEqualArrays(np.array([[1]]), pr.apply(pic))
+
+    def test_apply_stride_2_kernel_1x1(self):
+        pic = np.array([[1, 0, 2],
+                        [0, 0, 0],
+                        [3, 0, 4]])
+        pr = PhotoreceptorImageConverter(np.array([[2]]), pic.shape, 4)
+        self.assertEqualArrays(np.array([[2, 4],
+                                         [6, 8]]), pr.apply(pic))
+
+
 
 
 if __name__ == '__main__':
